caad3d333c5ea931b9aa984194afb2f45508ed4f
JenniferAbarca <jenniff.1999@gmail.com>
2026-06-23 15:41:32 -0600

    leccion 6

---
+++ src/app/pages/anime/anime-page.component.ts
@@ -0,0 +1,22 @@
+import { Component, signal } from '@angular/core';
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
+
+@Component({
+  selector: 'app-anime-page',
+  templateUrl: './anime-page.component.html'
+})
+export class AnimeComponent { 
+  name = signal(''); 
+  power = signal(0);
+  characters = signal<Character[]>([
+    { id: 1, name: 'Goku ', power: 9001},
+    { id: 2, name: 'Vegeta ', power: 8000},
+    { id: 3, name: 'Picoro ', power: 3000},
+    { id: 4, name: 'Yamcha ', power: 500},
+  ])
+
+}
